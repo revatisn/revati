39039b29a4ffd0cb6532e5a09f91f2852702c0fb
Revati S N <114300695+revatisn@users.noreply.github.com>
2022-09-27 14:46:01 +0530

    Add files via upload
    Scanner class is used to get user input, and it is found in the java.util package.

---
+++ InputDemo.java
@@ -0,0 +1,14 @@
+package edu.Scanner;
+
+import java.util.Scanner;
+
+public class InputDemo {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Enter the number");
+		Scanner object = new Scanner(System.in);
+
+	}
+
+}
