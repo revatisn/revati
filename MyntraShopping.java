acb787567f03d810876995b2af5707025213d306
Revati S N <114300695+revatisn@users.noreply.github.com>
2022-09-27 13:25:04 +0530

    Add files via upload

---
+++ MyntraShopping.java
@@ -0,0 +1,21 @@
+package in.edu.Shopping;
+
+public class MyntraShopping {
+	int cost;
+	String category;
+	String occasion;
+	
+	void display() {
+		
+		System.out.println("Welcome Customer:)");
+	}
+
+	public static void main(String[] args) {
+		MyntraShopping obj=new MyntraShopping();
+		obj.display();
+		
+	}
+
+}
+
+
